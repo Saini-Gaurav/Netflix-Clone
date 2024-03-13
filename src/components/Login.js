ac77c918a357de9251e76db6308563edbf09c660
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-03-13 17:28:57 +0530

    Login form is designed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
     <div>
-        Login
+        <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9134db96-10d6-4a64-a619-a21da22f8999/a449fabb-05e4-4c8a-b062-b0bec7d03085/IN-en-20240115-trifectadaily-perspective_alpha_website_small.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign in</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
