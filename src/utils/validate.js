52276e9fa2695cf703979d9bc9bd9cab9a530eab
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-03-14 14:34:17 +0530

    Validate the Sign In & Sign Up form data

---
+++ src/utils/validate.js
@@ -0,0 +1,13 @@
+export const checkValidData = (email, password, name)=>{
+
+    const isNameValid = /^[0-9A-Za-z]{6,16}$/.test(name)
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    
+    if(!isNameValid) {return "Name is not valid"};
+    if(!isEmailValid){ return "Email ID is not valid"};
+    if(!isPasswordValid){ return "Password is not valid"};
+
+    return null;
+
+}
